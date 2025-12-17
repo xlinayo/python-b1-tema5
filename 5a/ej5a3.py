198b3ef6044afec10ae38bd80f022f43674b7c84
xlinayo <xeila2012@gmail.com>
2025-12-17 18:50:33 +0100

    XEILA LIÑAYO Update ej5a3.py

--- 5a/ej5a3.py
+++ 5a/ej5a3.py
@@ -56,6 +56,12 @@ Exemple:
 
 # Corret and overwrite class SafeWalletCredentials here 
 class SafeWalletCredentials:
+    def __init__(self, password):
+        self.__password = password
+    def set_password(self, password):
+        self.__password = password
+    def get_password(self):
+        return self.__password
     pass
 
 
